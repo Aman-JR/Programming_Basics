3369f92621df1de1d97f3eb0ed65c6c7606275f2
Aman Chauhan <69090733+Aman-JR@users.noreply.github.com>
2023-10-09 21:05:00 +0530

    D10

--- Python/BinaryTree/BTfromIOandPO.py
+++ Python/BinaryTree/BTfromIOandPO.py
@@ -1,3 +1,29 @@
 
 #! Create a BT using pre and in order
+# # Steps to follow:
+# 1. Find the root
+# 2. Find in-order of both the left and the right sub Tree
+# 3. Find pre order of the right and the left subtree
+# 4. use recursion to build the left and the right subtree
+# 5. create the left and right with the help of root and return the root
+
+
+
+class BinaryTreeNode:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def BTfromIOandPo(io,po):
+    pass
+    
+
+print("Enter the In-order : ")
+io = [int(i) for i in input().split()]
+
+print("Enter the Pre-order : ")
+
+
+
 
